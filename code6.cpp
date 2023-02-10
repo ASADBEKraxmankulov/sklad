e671e589d1cf720487d431cbaec2215ca1ff7185
ASADBEKraxmankulov <124910214+ASADBEKraxmankulov@users.noreply.github.com>
2023-02-10 20:23:39 +0500

    Add files via upload

---
+++ code6.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int num;
+	double sq_root;
+	for (num = 1; num < 2000; num++) {
+		sq_root = sqrt((double)num);
+		cout << num << " " << sq_root << '\n';
+	}
+
+}
+
+
+
