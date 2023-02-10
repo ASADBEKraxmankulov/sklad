2a7892b1e5010c663404519aef5cdaefcdf266e5
ASADBEKraxmankulov <124910214+ASADBEKraxmankulov@users.noreply.github.com>
2023-02-10 20:28:55 +0500

    Add files via upload

---
+++ .github/workflows/code12.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int balance;
+	int *balptr;
+	int value;
+	balance = 3200;
+	balptr = &balance;
+	value = *balptr;
+	cout << "teng balans:" << value << '\n';
+	return 0;
+
+}
