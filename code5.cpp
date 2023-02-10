9051cf61188914042d741d0a028dcaea88a12771
ASADBEKraxmankulov <124910214+ASADBEKraxmankulov@users.noreply.github.com>
2023-02-10 20:23:12 +0500

    Add files via upload

---
+++ code5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int x;
+	for (x = 0; x < 6; x++) {
+		if (x == 1) cout << "x birga teng. /n";
+		else if (x == 2) cout << "x ikkiga teng. /n";
+		else if (x == 3) cout << "x uchga teng./n";
+		else if (x == 4) cout << " x tortga teng. /n";
+		else if (x == 5) cout << " x beshga teng. /n";
+		else if (x == 6) cout << " x oltiga teng. /n";
+		else cout << "x 1 dan 6 gacha teng./n";
+		
+	}
+	return 0;
+}
