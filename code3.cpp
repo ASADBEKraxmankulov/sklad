79a3db6202e2352a8ee0340a4ce025020c71cc0a
ASADBEKraxmankulov <124910214+ASADBEKraxmankulov@users.noreply.github.com>
2023-02-10 20:22:14 +0500

    Add files via upload

---
+++ code3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << " ikkita sonni kiriting";
+	cin >> a >> b;
+	if (b) cout << a / b << '/n';
+	if (b) cout << a * b << '/n';
+	if (b) cout << a - b << '/n';
+	if (b) cout << "nolga bolish mumkin emas/ n";
+	return 0;
+
+}
