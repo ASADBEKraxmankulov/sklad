91cdf59c660bf953597c217a77ce07dcdef203c1
ASADBEKraxmankulov <124910214+ASADBEKraxmankulov@users.noreply.github.com>
2023-02-10 20:43:33 +0500

    Add files via upload

---
+++ code27.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+void drill();
+int hisob;
+int num_right;
+int main()
+{
+	cout << "nechta mashqlar ishlaysiz:";
+	cin >> hisob;
+	num_right = 0;
+	do {
+		drill(); hisob--;
+	} while (hisob);
+		cout<< "siz" << num_right << "togri javob berdingiz.\n";
+		return 0;
+	}
+	
+
+
+
+	void drill()
+	{
+		int count;
+		int a, b, ans;
+		a = rand() % 100;
+		b = rand() % 100;
+		for (count = 0; count < 3; count++) {
+			cout << "nechta boladi" << a << "+" << b << "?";
+			cin >> ans;
+			if (ans == a + b) {
+				cout << "togri\n";
+				num_right++;
+				return;
+
+			}
+			cout << "siz ozingizni hamma imkoniyatlaringizdan foydalandingiz.\n";
+			cout << "javob teng" << a + b << '\n';
+	}
+	
+}
+
---
+++ code31.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+void f1();
+int main()
+{
+	char str[] = "bu massiv str funksiya main().";
+	cout << str << '\n';
+	f1();
+	cout << str << '\n';
+	return 0;
+}
+
+void f1()
+{
+	char str[80];
+	cout << "hohlagan qatoringizni tanlang:";
+	cin >> str;
+	cout << str << '\n';
+}
