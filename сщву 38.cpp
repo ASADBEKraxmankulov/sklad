258cbd1c2b4703dbfaaa940eec5e3b0787dc5f67
AsadbekRahmonkulov <124910214+ASADBEKraxmankulov@users.noreply.github.com>
2023-02-11 21:02:21 +0500

    Add files via upload

---
+++ сщву 38.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+unsigned int i;
+unsigned int delay;
+int main()
+{
+	register unsigned int j;
+	long start, end;
+	start = clock();
+	for (delay = 0; delay < 50; delay++)
+		for (i = 0; i < 64000000; i++);
+	end = clock();
+	cout << "tikilash soni";
+	cout << end - start << '\n';
+	start = clock();
+	for (delay = 0; delay < 50; delay++)
+		for (j = 0; j < 64000000; j++);
+	end = clock();
+	cout << "tikilash soni:";
+	cout << end - start << '\n';
+	return 0;
+}
